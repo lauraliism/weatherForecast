f57eb39b56ee952af4a9a2d17afbcf717f898bce
Laura Liis Metsvaht <laura.liis.metsvaht@gmail.com>
2017-09-25 01:03:30 +0300

    Initial commit

---
+++ src/test/java/httpUtilityTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+import org.openweathermap.api.WeatherForecast;
+
+/**
+ * Created by lauraliismetsvaht on 11/09/2017.
+ */
+public class httpUtilityTest {
+
+	WeatherForecast weatherForecast = new WeatherForecast();
+	String mockCityName = "Tartu";
+	String mockCountryCode = "EE";
+	Integer mockForecastLengthInDays = 3;
+
+	@Test
+	public void testConnectionToWeatherAPI() {
+	}
+
+	@Test
+	public void doesGetCurrentTemperatureReturnTemperature() {
+		weatherForecast.getCurrentTemperature(mockCityName, mockCountryCode);
+	}
+
+	@Test
+	public void doesGetHighestTemperatureForLastThreeDaysReturnForecastOfThreeDays() {
+		weatherForecast.getHighestTemperatureForLastThreeDays(mockCityName, mockCountryCode, mockForecastLengthInDays);
+	}
+
+	@Test
+	public void getReturnLowestTemperatureForLastThreeDaysDoesReturnForecastOfThreeDays() {
+		weatherForecast.getLowestTemperatureForLastThreeDays(mockCityName, mockCountryCode, mockForecastLengthInDays);
+	}
+
+	@Test
+	public void doesGetCityCoordinatesReturnCoordinates() {
+		weatherForecast.getCityCoordinates(mockCityName, mockCountryCode);
+	}
+
+}
